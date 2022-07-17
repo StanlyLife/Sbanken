97aa9eafcdd36c387172774617d4af0d217d4f0b
Stian have <stianhave@hotmail.com>
2022-07-17 22:12:18 +0200

    added more urls

--- sbanken/src/utils/sbankenApi.ts
+++ sbanken/src/utils/sbankenApi.ts
@@ -1,1 +1,3 @@
-export const baseUrl = 'https://publicapi.sbanken.no/apibeta';
+const version = 'v2';
+export const baseUrl = 'https://publicapi.sbanken.no/apibeta/';
+export const getAccountsUrl = `api/${version}/Accounts`;
