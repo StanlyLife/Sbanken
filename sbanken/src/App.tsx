86c938f4c8f868cae97405702d4599372e9fdf88
Stian have <stianhave@hotmail.com>
2022-07-17 18:55:23 +0200

    Added temporary resolver for bearer token

--- sbanken/src/App.tsx
+++ sbanken/src/App.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import apiKey, { apiSecret } from './utils/secrets';
+const axios = require('axios');
 function App() {
+    useEffect(() => {
+        initializer();
+    }, []);
+    const initializer = async () => {
+        const identityServerUrl = 'https://auth.sbanken.no/identityserver/connect/token';
+        const basicAuthentationHeaderValue = btoa(encodeURIComponent(apiKey) + ':' + encodeURIComponent(apiSecret));
+        const headers = {
+            Authorization: 'Basic ' + basicAuthentationHeaderValue,
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
+
+        const axiosConfig = {
+            method: 'post',
+            url: identityServerUrl,
+            headers: headers,
+            data: 'grant_type=client_credentials',
+        };
+        axios(axiosConfig).then(
+            (res: any) => {
+                console.log('res');
+                console.log(res.data.access_token);
+            },
+            (err: any) => {
+                console.log('Error');
+                console.log(err);
+            },
+        );
+    };
+
     return (
         <div className='App'>
             <header className='App-header'>
