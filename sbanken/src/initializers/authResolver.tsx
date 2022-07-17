66306cb58d4189adec0b2fcb27296829fceaa042
Stian have <stianhave@hotmail.com>
2022-07-17 22:11:48 +0200

    Refactored authentication

---
+++ sbanken/src/initializers/authResolver.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import apiKey, { apiSecret } from '../utils/secrets';
+const axios = require('axios');
+
+export const authResolver = () => {
+    useEffect(() => {
+        initializer();
+    }, []);
+    const initializer = async () => {
+        const identityServerUrl = 'https://auth.sbanken.no/identityserver/connect/token';
+        const basicAuthentationHeaderValue = btoa(encodeURIComponent(apiKey) + ':' + encodeURIComponent(apiSecret));
+        const headers = {
+            Authorization: 'Basic ' + basicAuthentationHeaderValue,
+            Accept: 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        };
+
+        const axiosConfig = {
+            method: 'post',
+            url: identityServerUrl,
+            headers: headers,
+            data: 'grant_type=client_credentials',
+        };
+        axios(axiosConfig).then(
+            (res: any) => {
+                console.log(res.data.access_token);
+            },
+            (err: any) => {
+                console.log('error retrieving auth token');
+                console.log(err);
+            },
+        );
+    };
+};
