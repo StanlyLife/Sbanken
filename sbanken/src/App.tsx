3a2db1e960740e3603799c0d319fecff3fcdd848
Stian have <stianhave@hotmail.com>
2022-07-17 22:11:00 +0200

    cleaned up app.tsx & added scss file

--- sbanken/src/App.tsx
+++ sbanken/src/App.tsx
@@ -1,48 +1,14 @@
-import React, { useEffect } from 'react';
-import apiKey, { apiSecret } from './utils/secrets';
-const axios = require('axios');
+import React from 'react';
+import { authResolver } from './initializers/authResolver';
+import './scss/main.scss';
 function App() {
-    useEffect(() => {
-        initializer();
-    }, []);
-    const initializer = async () => {
-        const identityServerUrl = 'https://auth.sbanken.no/identityserver/connect/token';
-        const basicAuthentationHeaderValue = btoa(encodeURIComponent(apiKey) + ':' + encodeURIComponent(apiSecret));
-        const headers = {
-            Authorization: 'Basic ' + basicAuthentationHeaderValue,
-            Accept: 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded',
-        };
-
-        const axiosConfig = {
-            method: 'post',
-            url: identityServerUrl,
-            headers: headers,
-            data: 'grant_type=client_credentials',
-        };
-        axios(axiosConfig).then(
-            (res: any) => {
-                console.log('res');
-                console.log(res.data.access_token);
-            },
-            (err: any) => {
-                console.log('Error');
-                console.log(err);
-            },
-        );
-    };
-
+    authResolver();
     return (
-        <div className='App'>
-            <header className='App-header'>
-                <p>
-                    Edit
-                    <code>src/App.js</code>
-                </p>
-                <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-                    Learn React
-                </a>
-            </header>
+        <div className='App-Root'>
+            <header className='App-header'></header>
+            <main>
+                <h1>app</h1>
+            </main>
         </div>
     );
 }
