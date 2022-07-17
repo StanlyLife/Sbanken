08327c8ac4e17343fbda1258904d2775a3351c66
Stian have <stianhave@hotmail.com>
2022-07-17 22:35:14 +0200

    added accounts to store

--- sbanken/src/stores/apiStore.js
+++ sbanken/src/stores/apiStore.js
@@ -3,7 +3,11 @@ import { devtools } from 'zustand/middleware';
 
 export const apiStore = create(
     devtools((set, get) => ({
+        //bearertoken
         bearerToken_store: '',
         setBearerToken_store: (data) => set({ bearerToken_store: data }, false, 'set bearer token'),
+        //Accounts
+        accounts_store: '',
+        setAccounts_store: (data) => set({ accounts_store: data }, false, 'set account data'),
     })),
 );
--- sbanken/src/stores/useApiStore.js
+++ sbanken/src/stores/useApiStore.js
@@ -1,5 +1,9 @@
+import { apiStore } from './apiStore';
 export const useApiStore = () => ({
-    // vehicle driver data
+    // Bearertoken
     bearerToken_store: apiStore((state) => state.bearerToken_store),
     setBearerToken_store: apiStore((state) => state.setBearerToken_store),
+    // Account
+    accounts_store: apiStore((state) => state.accounts_store),
+    setAccounts_store: apiStore((state) => state.setAccounts_store),
 });
