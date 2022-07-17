6e6d2c165c835d1ffc4b6336cae32d6a5e3e61a3
Stian have <stianhave@hotmail.com>
2022-07-17 22:35:32 +0200

    added account interface

---
+++ sbanken/src/interfaces/accountItem.ts
@@ -0,0 +1,11 @@
+export interface AccountItem {
+    accountId: string;
+    accountNumber: string;
+    ownerCustomerId: string;
+    name: string;
+    accountType: string;
+    available: number;
+    balance: number;
+    creditLimit: number;
+}
+export default AccountItem;
