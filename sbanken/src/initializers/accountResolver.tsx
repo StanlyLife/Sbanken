773358e93d449caffc68e5d0f242f19ee94f5013
Stian have <stianhave@hotmail.com>
2022-07-18 20:05:49 +0200

    removed console.logs

--- sbanken/src/initializers/accountResolver.tsx
+++ sbanken/src/initializers/accountResolver.tsx
@@ -7,14 +7,12 @@ const axios = require('axios');
 export const AccountResolver = () => {
     const { bearerToken_store, setAccounts_store, accounts_store } = useApiStore();
     useEffect(() => {
-        console.log(bearerToken_store);
         initializer();
     }, [bearerToken_store]);
     const initializer = async () => {
         const url = baseUrl + getAccountsUrl;
         axios(getAxiosConfig('get', url, bearerToken_store)).then(
             (res: any) => {
-                console.log('got accounts');
                 setAccounts_store(res.data.items);
             },
             (err: any) => {
@@ -28,7 +26,6 @@ export const AccountResolver = () => {
             <h1>
                 {accounts_store &&
                     accounts_store.map((account: AccountItem) => {
-                        console.log(account);
                         return (
                             <>
                                 <p>{account.accountNumber}</p>
