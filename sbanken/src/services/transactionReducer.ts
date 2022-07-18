d8deb0c1403aae6082ed926bb88dee4bf1ac44e2
Stian have <stianhave@hotmail.com>
2022-07-18 20:58:04 +0200

    implemented blackList functionality Blacklisted names will not be displayed in transactions

--- sbanken/src/services/transactionReducer.ts
+++ sbanken/src/services/transactionReducer.ts
@@ -1,27 +1,33 @@
 import { transactionTypes } from '../enums/transactionTypes';
+import { TransactionBlackList } from '../utils/blackList';
 
 export const transactionReducer = (arr: Array<any>) => {
     return arr.reduce((acc: any, curr: any) => {
-        if (curr.transactionTypeCode === 714 && curr.cardDetails) {
-            if (acc[curr.cardDetails.merchantName]) {
-                //update object
-                acc[curr.cardDetails.merchantName].amount += curr.amount;
-                acc[curr.cardDetails.merchantName].count += 1;
-                return acc;
+        const currentName =
+            curr.transactionTypeCode === 714 && curr.cardDetails
+                ? curr.cardDetails.merchantName
+                : transactionTypes[curr.transactionTypeCode];
+        if (!TransactionBlackList.includes(currentName)) {
+            if (curr.transactionTypeCode === 714 && curr.cardDetails) {
+                if (acc[curr.cardDetails.merchantName]) {
+                    //update object
+                    acc[curr.cardDetails.merchantName].amount += curr.amount;
+                    acc[curr.cardDetails.merchantName].count += 1;
+                    return acc;
+                }
+                //add new value to object
+                acc[curr.cardDetails.merchantName] = { amount: curr.amount, count: 1 };
+            } else {
+                if (acc[transactionTypes[curr.transactionTypeCode]]) {
+                    //update object
+                    acc[transactionTypes[curr.transactionTypeCode]].amount += curr.amount;
+                    acc[transactionTypes[curr.transactionTypeCode]].count += 1;
+                    return acc;
+                }
+                //add new value to object
+                acc[transactionTypes[curr.transactionTypeCode]] = { amount: curr.amount, count: 1 };
             }
-            //add new value to object
-            acc[curr.cardDetails.merchantName] = { amount: curr.amount, count: 1 };
-            return acc;
-        } else {
-            if (acc[transactionTypes[curr.transactionTypeCode]]) {
-                //update object
-                acc[transactionTypes[curr.transactionTypeCode]].amount += curr.amount;
-                acc[transactionTypes[curr.transactionTypeCode]].count += 1;
-                return acc;
-            }
-            //add new value to object
-            acc[transactionTypes[curr.transactionTypeCode]] = { amount: curr.amount, count: 1 };
-            return acc;
         }
+        return acc;
     }, {});
 };
