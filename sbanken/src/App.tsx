dab706eb9db2cf43a0260b4662ab7d77ac726f67
Stian have <stianhave@hotmail.com>
2022-07-19 19:56:24 +0200

    added required initializers

--- sbanken/src/App.tsx
+++ sbanken/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { AccountResolver } from './initializers/accountResolver';
 import { authResolver } from './initializers/authResolver';
+import { TransactionResolver } from './initializers/transactionResolver';
+import { Home } from './routes/home/home';
 import './scss/main.scss';
+import 'react-circular-progressbar/dist/styles.css';
 function App() {
     authResolver();
     return (
         <div className='App-Root'>
             <header className='App-header'></header>
             <main>
-                <h1>app</h1>
+                <Home />
+                <AccountResolver />
+                <TransactionResolver />
             </main>
         </div>
     );
