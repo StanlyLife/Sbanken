37a1122547e462a2c26a9aa5d278f7c5a2c6d012
Stian have <stianhave@hotmail.com>
2022-07-19 20:28:06 +0200

    added end & start date

--- sbanken/src/stores/dateStore.js
+++ sbanken/src/stores/dateStore.js
@@ -1,10 +1,20 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
-
+const selectionRange = {
+    startDate: new Date(),
+    endDate: new Date(),
+    key: 'selection',
+};
 export const dateStore = create(
     devtools((set, get) => ({
         //date
-        date_store: '',
+        date_store: [selectionRange],
         setDate_store: (data) => set({ date_store: data }, false, 'set date range'),
+        //start date
+        startDate_store: undefined,
+        setStartDate_store: (data) => set({ startDate_store: data }, false, 'set start date'),
+        //end date
+        endDate_store: undefined,
+        setEndDate_store: (data) => set({ endDate_store: data }, false, 'set end date'),
     })),
 );
--- sbanken/src/stores/useDateStore.js
+++ sbanken/src/stores/useDateStore.js
@@ -3,4 +3,10 @@ export const useDateStore = () => ({
     // date
     date_store: dateStore((state) => state.date_store),
     setDate_store: dateStore((state) => state.setDate_store),
+    // startdate
+    startDate_store: dateStore((state) => state.startDate_store),
+    setStartDate_store: dateStore((state) => state.setStartDate_store),
+    // enddate
+    endDate_store: dateStore((state) => state.endDate_store),
+    setEndDate_store: dateStore((state) => state.setEndDate_store),
 });
