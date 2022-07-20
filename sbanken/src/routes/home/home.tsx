871ea650a06e5640c05eee4c8587e6378061d497
Stian have <stianhave@hotmail.com>
2022-07-20 23:22:15 +0200

    added header

--- sbanken/src/routes/home/home.tsx
+++ sbanken/src/routes/home/home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { OverviewProgress } from './overviewProgress/overviewProgress';
+import './home.scss';
 export const Home = () => {
     return (
-        <>
+        <div className='home-component'>
+            <header>
+                <h1 className='title'>Sbanken forbruk</h1>
+            </header>
             <OverviewProgress />
-        </>
+        </div>
     );
 };
