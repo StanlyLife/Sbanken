b2627fa6fbfc095f74756457974082793a0beea2
Stian have <stianhave@hotmail.com>
2022-07-20 23:04:11 +0200

    Added api update when 'selected date' is changed

--- sbanken/src/initializers/transactionResolver.tsx
+++ sbanken/src/initializers/transactionResolver.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
 import { transactionReducer } from '../services/transactionReducer';
 import { useApiStore } from '../stores/useApiStore';
+import { useDateStore } from '../stores/useDateStore';
 import { getAxiosConfig } from '../utils/getAxiosConfig';
 import { baseUrl, getTransactionsUrl } from '../utils/sbankenApi';
 const axios = require('axios');
 export const TransactionResolver = () => {
     const { bearerToken_store, accounts_store, setTransactions_store, transactions_store, setTransaction_data_store } =
         useApiStore();
+    const { date_store, startDate_store, endDate_store } = useDateStore();
     useEffect(() => {
         if (transactions_store) {
             const result = transactionReducer(transactions_store.items);
@@ -16,16 +18,20 @@ export const TransactionResolver = () => {
     useEffect(() => {
         if (accounts_store) initializer();
         if (!accounts_store) console.log('accounts not set!');
-    }, [accounts_store]);
+    }, [accounts_store, date_store]);
     const initializer = async () => {
-        const url = baseUrl + getTransactionsUrl + accounts_store[0].accountId;
+        const earliestDate = startDate_store < endDate_store ? startDate_store : endDate_store;
+        const latestDate = earliestDate < endDate_store ? endDate_store : startDate_store;
+        const url =
+            baseUrl + getTransactionsUrl + accounts_store[0].accountId + `/?startDate=${earliestDate}&endDate=${latestDate}`;
         axios(getAxiosConfig('get', url, bearerToken_store)).then(
             (res: any) => {
                 setTransactions_store(res.data);
             },
             (err: any) => {
-                console.log('error retrieving auth token');
+                console.log('error transaction data');
                 console.log(err);
+                console.log(`status: ${err.response.status} - message: ${err.response.statusText}`);
             },
         );
     };
