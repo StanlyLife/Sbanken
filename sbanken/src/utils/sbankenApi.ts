042d94cc18bbf2ebb42b7f675eae14156d002ef1
Stian have <stianhave@hotmail.com>
2022-07-20 23:03:02 +0200

    added api urls

--- sbanken/src/utils/sbankenApi.ts
+++ sbanken/src/utils/sbankenApi.ts
@@ -1,3 +1,5 @@
 const version = 'v2';
 export const baseUrl = 'https://publicapi.sbanken.no/apibeta/';
 export const getAccountsUrl = `api/${version}/Accounts`;
+export const getPaymentsUrl = `api/${version}/Payments/`; //AccountId at the end
+export const getTransactionsUrl = `api/${version}/Transactions/`; //AccountId at the end
