9833fe659908e5d9937b2eeb645eeabe8ad09251
Stian have <stianhave@hotmail.com>
2022-07-22 22:48:43 +0200

    implemented transactionSummaryGrid

--- sbanken/src/routes/home/overviewProgress/grids/transactionSummaryGrid/transactionSummaryGrid.tsx
+++ sbanken/src/routes/home/overviewProgress/grids/transactionSummaryGrid/transactionSummaryGrid.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import './transactionSummaryGrid.scss';
+import { useApiStore } from '../../../../../stores/useApiStore';
+import { transactionReducedDataToGridData } from '../../../../../services/transactionReducedDataToGridData';
 
 export const TransactionSummaryGrid = () => {
-    const [rowData] = useState([
-        { make: 'Toyota', model: 'Celica', price: 35000 },
-        { make: 'Ford', model: 'Mondeo', price: 32000 },
-        { make: 'Porsche', model: 'Boxster', price: 72000 },
-    ]);
+    const { transaction_data_store, transactions_store } = useApiStore();
+    const [rowData, setRowData] = useState<any>();
+    useEffect(() => {
+        const data = Object.entries(transaction_data_store).map((e) => ({ [e[0]]: e[1] }));
+        const gridData = transactionReducedDataToGridData(data);
+        setRowData(gridData);
+    }, [transaction_data_store]);
+    useEffect(() => {
+        console.log(transactions_store.items);
+    }, [transactions_store]);
 
-    const [columnDefs] = useState([{ field: 'make' }, { field: 'model' }, { field: 'price' }]);
+    const [columnDefs] = useState([
+        { field: 'name', flex: 1 },
+        { field: 'amount', flex: 1 },
+        { field: 'count', flex: 1 },
+    ]);
 
     return (
         <div className='transaction-summary-grid-component'>
-            <div className='ag-theme-alpine' style={{ height: 400, width: 600 }}>
-                <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
-            </div>
+            {transaction_data_store && (
+                <div className='ag-theme-alpine' id='transaction-summary-grid' style={{ height: 400 }}>
+                    <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+                </div>
+            )}
         </div>
     );
 };
