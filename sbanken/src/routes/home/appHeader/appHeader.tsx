a942e3f187a18a55e82b301c1c7045960809b641
Stian have <stianhave@hotmail.com>
2022-07-22 21:27:59 +0200

    added calendar modal

--- sbanken/src/routes/home/appHeader/appHeader.tsx
+++ sbanken/src/routes/home/appHeader/appHeader.tsx
@@ -1,7 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import './appHeader.scss';
+import { DateSelector } from './DateSelector/dateSelector';
+import { useState } from 'react';
 export const AppHeader = () => {
+    const [displayCalendar, setDisplayCalendar] = useState<boolean>(false);
     return (
-        <header>
-            <h1 className='title'>Sbanken forbruk</h1>
-        </header>
+        <>
+            <header className='app-header-component'>
+                <h1 className='title'>Sbanken forbruk</h1>
+                <button className='calendar-button' onClick={() => setDisplayCalendar((prev: boolean) => !prev)}>
+                    <FontAwesomeIcon icon={faCalendarAlt} />
+                </button>
+            </header>
+            {displayCalendar && (
+                <>
+                    <DateSelector setDisplayCalendar={setDisplayCalendar} />
+                </>
+            )}
+        </>
     );
 };
