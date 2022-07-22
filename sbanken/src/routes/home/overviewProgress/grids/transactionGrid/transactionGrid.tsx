cc5165607afb1fba2a87793e6b24fdae3f1a3cdf
Stian have <stianhave@hotmail.com>
2022-07-22 22:48:31 +0200

    added transactionGrid

---
+++ sbanken/src/routes/home/overviewProgress/grids/transactionGrid/transactionGrid.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { useApiStore } from '../../../../../stores/useApiStore';
+
+export const TransactionGrid = () => {
+    const { transactions_store } = useApiStore();
+    const [rowData, setRowData] = useState<any>();
+
+    useEffect(() => {
+        setRowData(transactions_store.items);
+    }, [transactions_store]);
+
+    const [columnDefs] = useState([
+        { field: 'accountingDate', flex: 1, sortable: true },
+        { field: 'amount', flex: 1, sortable: true },
+        { field: 'transactionType', flex: 1, sortable: true },
+        { field: 'text', flex: 1, sortable: true },
+    ]);
+
+    return (
+        <div className='transaction-summary-grid-component'>
+            {transactions_store && (
+                <div className='ag-theme-alpine' id='transaction-summary-grid' style={{ height: 400 }}>
+                    <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+                </div>
+            )}
+        </div>
+    );
+};
