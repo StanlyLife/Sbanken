faa1f4f83df3e20d6d5e098928b3615620e160d8
Stian have <stianhave@hotmail.com>
2022-07-22 22:41:17 +0200

    Changes to compliment sidebar and new layout

--- sbanken/src/routes/home/home.tsx
+++ sbanken/src/routes/home/home.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { OverviewProgress } from './overviewProgress/overviewProgress';
 import './home.scss';
 import { AppHeader } from './appHeader/appHeader';
+import { AppSidebar } from './appSidebar/appSidebar';
 export const Home = () => {
     return (
         <div className='home-component'>
-            <AppHeader />
-            <OverviewProgress />
+            <div className='home-component-sidebar'>
+                <AppSidebar />
+            </div>
+            <div className='home-component-main'>
+                <div className='home-component-header'>
+                    <AppHeader />
+                </div>
+                <div className='home-component-overview'>
+                    <OverviewProgress />
+                </div>
+            </div>
         </div>
     );
 };
