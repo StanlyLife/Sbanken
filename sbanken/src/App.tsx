c5cdd234e69052ad87d3ccf5a23cac1773c9822f
Stian have <stianhave@hotmail.com>
2022-07-23 13:48:44 +0200

    added fonts

--- sbanken/src/App.tsx
+++ sbanken/src/App.tsx
@@ -4,6 +4,7 @@ import { authResolver } from './initializers/authResolver';
 import { TransactionResolver } from './initializers/transactionResolver';
 import { Home } from './routes/home/home';
 import './scss/main.scss';
+import './scss/fonts.scss';
 import 'react-circular-progressbar/dist/styles.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
