1c4d3605afef2d91f48eeffe38672ddca56e6542
Stian have <stianhave@hotmail.com>
2022-07-23 13:24:26 +0200

    added links

--- sbanken/src/routes/home/appSidebar/appSidebar.tsx
+++ sbanken/src/routes/home/appSidebar/appSidebar.tsx
@@ -1,7 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import './appSidebar.scss';
+import { NavLink } from 'react-router-dom';
 export const AppSidebar = () => {
     return (
         <aside className='app-sidebar-component'>
-            <h1>test</h1>
+            <div className='profile'>
+                <div className='title'>
+                    <h1>Sbanken</h1>
+                    <h1>Forbruk</h1>
+                    <div className='border'></div>
+                </div>
+                <div className='picture'>
+                    <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                </div>
+                <div className='name'>
+                    <p>Stian Håve</p>
+                </div>
+            </div>
+            <div className='links'>
+                <NavLink className={(navData) => (navData.isActive ? 'link active' : 'link')} to='/'>
+                    <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+                    <p>Home</p>
+                </NavLink>
+            </div>
         </aside>
     );
 };
--- sbanken/src/routes/home/home.tsx
+++ sbanken/src/routes/home/home.tsx
@@ -6,22 +6,22 @@ import { HomeWrapper } from './homeWrapper/homeWrapper';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export const Home = () => {
     return (
-        <div className='home-component'>
-            <div className='home-component-sidebar'>
-                <AppSidebar />
-            </div>
-            <div className='home-component-main'>
-                <div className='home-component-header'>
-                    <AppHeader />
+        <BrowserRouter>
+            <div className='home-component'>
+                <div className='home-component-sidebar'>
+                    <AppSidebar />
                 </div>
-                <div className='home-component-overview'>
-                    <BrowserRouter>
+                <div className='home-component-main'>
+                    <div className='home-component-header'>
+                        <AppHeader />
+                    </div>
+                    <div className='home-component-overview'>
                         <Routes>
                             <Route path='/' element={<HomeWrapper />} />
                         </Routes>
-                    </BrowserRouter>
+                    </div>
                 </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 };
