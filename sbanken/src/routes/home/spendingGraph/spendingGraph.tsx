4422dbc77590271fabb2d682c58ad141358fc422
Stian have <stianhave@hotmail.com>
2022-07-23 18:06:57 +0200

    created spendingGraph

---
+++ sbanken/src/routes/home/spendingGraph/spendingGraph.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { transactionReducedDataToGridData } from '../../../services/transactionReducedDataToGridData';
+import { useApiStore } from '../../../stores/useApiStore';
+import { transactionItem } from '../spendingOverview/transactionItem';
+import './spendingGraph.scss';
+
+export const SpendingGraph = () => {
+    const { transaction_data_store } = useApiStore();
+    const [data, setData] = useState<Array<transactionItem>>();
+    const [totalSpending, setTotalSpending] = useState<number>();
+    const getTotalAmountOfSpending = () => {
+        if (data)
+            return data?.reduce((acc: number, curr: transactionItem) => {
+                console.log(curr.amount);
+                if (curr && curr.amount < 0) acc += curr.amount;
+                return acc;
+            }, 0);
+        return 0;
+    };
+    useEffect(() => {
+        const newData = Object.entries(transaction_data_store).map((e) => ({ [e[0]]: e[1] }));
+        const transactionData = transactionReducedDataToGridData(newData);
+        setData(transactionData);
+    }, [transaction_data_store]);
+
+    useEffect(() => {
+        setTotalSpending(getTotalAmountOfSpending());
+    }, [data]);
+    return (
+        <div className='spending-graph-component'>
+            <div className='transaction-wrapper'>
+                {data &&
+                    totalSpending &&
+                    data.map((transaction: transactionItem) => {
+                        //We do not want to display money in
+                        const transactionIsCost = transaction.amount < 0;
+                        if (transactionIsCost) {
+                            const mostSpentCategory = data[0].amount;
+                            //Percentage based of most spent category
+                            //We want the top category to be 100% width
+                            const percentageOfMostSpentTransaction = Math.round(
+                                (Math.abs(transaction.amount) / Math.abs(mostSpentCategory)) * 100,
+                            );
+                            const percentageOfTotalSpending = Math.round(
+                                (Math.abs(transaction.amount) / Math.abs(totalSpending)) * 100,
+                            );
+                            return (
+                                <div className='transaction'>
+                                    <div className='data'>
+                                        <p className='name'>{transaction.name}</p>
+                                        <p>
+                                            {transaction.amount}kr / {percentageOfTotalSpending}%
+                                        </p>
+                                    </div>
+                                    <div
+                                        className='transaction-line'
+                                        title={`${percentageOfMostSpentTransaction}%`}
+                                        style={{ width: `${percentageOfMostSpentTransaction}%` }}
+                                    ></div>
+                                </div>
+                            );
+                        }
+                        return <></>;
+                    })}
+            </div>
+        </div>
+    );
+};
