9e8a57cd69807ee17e450785c6bfb48b87603dba
Stian have <stianhave@hotmail.com>
2022-07-23 19:25:16 +0200

    removed placeholder contents and added cards

--- sbanken/src/routes/home/appHeader/appHeader.tsx
+++ sbanken/src/routes/home/appHeader/appHeader.tsx
@@ -3,16 +3,17 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
 import './appHeader.scss';
 import { DateSelector } from './DateSelector/dateSelector';
 import { useState } from 'react';
+import { Cards } from '../cards/cards';
 export const AppHeader = () => {
     const [displayCalendar, setDisplayCalendar] = useState<boolean>(false);
     return (
         <>
             <header className='app-header-component'>
-                <h1 className='title'>Sbanken forbruk</h1>
+                <Cards />
+
                 <button className='calendar-button' onClick={() => setDisplayCalendar((prev: boolean) => !prev)}>
                     <FontAwesomeIcon icon={faCalendarAlt} />
                 </button>
-                <span className='border-bottom'></span>
             </header>
             {displayCalendar && <DateSelector setDisplayCalendar={setDisplayCalendar} />}
         </>
