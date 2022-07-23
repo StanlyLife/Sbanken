a9c0b46dc6db7f4eea709d6f84fe282464efe40f
Stian have <stianhave@hotmail.com>
2022-07-23 19:40:08 +0200

    Changed calculation to respect blacklist

--- sbanken/src/routes/home/cards/moneyIn.tsx
+++ sbanken/src/routes/home/cards/moneyIn.tsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react';
+import { transactionReducedDataToGridData } from '../../../services/transactionReducedDataToGridData';
 import { useApiStore } from '../../../stores/useApiStore';
+import { transactionItem } from '../spendingOverview/transactionItem';
 
 export const MoneyIn = () => {
-    const { transactions_store } = useApiStore();
+    const { transaction_data_store } = useApiStore();
     const [moneyIn, setMoneyIn] = useState(0);
-    useEffect(() => {
-        if (transactions_store) {
-            const moneyIn = transactions_store.items.reduce((acc: number, curr: any) => {
-                if (curr.amount > 0) acc += curr.amount;
+    const [data, setData] = useState<any>();
+
+    const getTotalAmountOfSpending = () => {
+        if (data)
+            return data?.reduce((acc: number, curr: transactionItem) => {
+                if (curr && curr.amount > 0) acc += curr.amount;
                 return acc;
             }, 0);
-            setMoneyIn(Math.round(moneyIn));
-        }
-    }, [transactions_store]);
+        return 0;
+    };
+
+    useEffect(() => {
+        const newData = Object.entries(transaction_data_store).map((e) => ({ [e[0]]: e[1] }));
+        const transactionData = transactionReducedDataToGridData(newData);
+        setData(transactionData);
+    }, [transaction_data_store]);
+
+    useEffect(() => {
+        setMoneyIn(getTotalAmountOfSpending());
+    }, [data]);
 
     return (
         <>
             <div className='card money-in-component'>
                 <p className='key'>Penger inn</p>
-                <p className='value'>{moneyIn}kr</p>
+                <p className='value'>{Math.round(moneyIn)}kr</p>
             </div>
         </>
     );
--- sbanken/src/routes/home/cards/moneyOut.tsx
+++ sbanken/src/routes/home/cards/moneyOut.tsx
@@ -1,24 +1,37 @@
 import { useEffect, useState } from 'react';
+import { transactionReducedDataToGridData } from '../../../services/transactionReducedDataToGridData';
 import { useApiStore } from '../../../stores/useApiStore';
+import { transactionItem } from '../spendingOverview/transactionItem';
 
 export const MoneyOut = () => {
-    const { transactions_store } = useApiStore();
-    const [moneyOut, setMoneyOut] = useState<string>('0');
-    useEffect(() => {
-        if (transactions_store) {
-            const moneyOut = transactions_store.items.reduce((acc: number, curr: any) => {
-                if (curr.amount < 0) acc += curr.amount;
+    const { transaction_data_store } = useApiStore();
+    const [moneyOut, setMoneyOut] = useState(0);
+    const [data, setData] = useState<any>();
+
+    const getTotalAmountOfSpending = () => {
+        if (data)
+            return data?.reduce((acc: number, curr: transactionItem) => {
+                if (curr && curr.amount < 0) acc += curr.amount;
                 return acc;
             }, 0);
-            setMoneyOut(Math.round(moneyOut).toString().replace('-', ''));
-        }
-    }, [transactions_store]);
+        return 0;
+    };
+
+    useEffect(() => {
+        const newData = Object.entries(transaction_data_store).map((e) => ({ [e[0]]: e[1] }));
+        const transactionData = transactionReducedDataToGridData(newData);
+        setData(transactionData);
+    }, [transaction_data_store]);
+
+    useEffect(() => {
+        setMoneyOut(getTotalAmountOfSpending().toString().replace('-', ''));
+    }, [data]);
 
     return (
         <>
             <div className='card money-out-component'>
                 <p className='key'>Penger ut</p>
-                <p className='value'>{moneyOut}kr</p>
+                <p className='value'>{Math.round(moneyOut)}kr</p>
             </div>
         </>
     );
