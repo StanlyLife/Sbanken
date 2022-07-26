e8c64cec27d60a203021d08e4f4c2cc4d09db72a
Stian have <stianhave@hotmail.com>
2022-07-26 21:47:31 +0200

    initial commit for category progress

---
+++ sbanken/src/routes/categories/categoryProgress/categoryProgress.tsx
@@ -0,0 +1,8 @@
+import './categoryProgress.scss';
+export const CategoryProgress = () => {
+    return (
+        <div className='category-process-component'>
+            <h1>Category progress</h1>
+        </div>
+    );
+};
