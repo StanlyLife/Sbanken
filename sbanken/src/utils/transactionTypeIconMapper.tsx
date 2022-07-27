ddbc2954b544476d1e205a575b2fc650083c5787
Stian have <stianhave@hotmail.com>
2022-07-27 22:09:42 +0200

    added icons for new categories

--- sbanken/src/utils/transactionTypeIconMapper.tsx
+++ sbanken/src/utils/transactionTypeIconMapper.tsx
@@ -1,10 +1,22 @@
 import {
+    faAppleAlt,
     faArrowRightArrowLeft,
     faBasketShopping,
+    faBurger,
+    faBus,
     faCartShopping,
     faCreditCard,
+    faFaceLaughBeam,
+    faGamepad,
+    faGraduationCap,
+    faHouse,
     faMobileAlt,
     faMoneyBillTransfer,
+    faPaw,
+    faPiggyBank,
+    faQuestion,
+    faUtensilSpoon,
+    faWarning,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /*
@@ -20,8 +32,22 @@ export const TransactionTypeIconMapper = (t: string) => {
         Visa: <FontAwesomeIcon icon={faCreditCard} />,
         Overførsel: <FontAwesomeIcon icon={faArrowRightArrowLeft} />,
         Overføring: <FontAwesomeIcon icon={faMoneyBillTransfer} />,
+        //Categories
+        unknown: <FontAwesomeIcon icon={faQuestion} />,
+        food: <FontAwesomeIcon icon={faBurger} />,
+        recreation: <FontAwesomeIcon icon={faFaceLaughBeam} />,
+        transport: <FontAwesomeIcon icon={faBus} />,
+        games: <FontAwesomeIcon icon={faGamepad} />,
+        learning: <FontAwesomeIcon icon={faGraduationCap} />,
+        vipps: <FontAwesomeIcon icon={faMobileAlt} />,
+        other: <FontAwesomeIcon icon={faQuestion} />,
+        investment: <FontAwesomeIcon icon={faPiggyBank} />,
+        dining: <FontAwesomeIcon icon={faUtensilSpoon} />,
+        groceries: <FontAwesomeIcon icon={faBasketShopping} />,
+        home: <FontAwesomeIcon icon={faHouse} />,
+        pet: <FontAwesomeIcon icon={faPaw} />,
+        apple: <FontAwesomeIcon icon={faAppleAlt} />,
     };
     const icon = iconTypes[t];
-    return icon;
-    // return <FontAwesomeIcon icon={faWarning} />;
+    return icon || <FontAwesomeIcon icon={faWarning} />;
 };
