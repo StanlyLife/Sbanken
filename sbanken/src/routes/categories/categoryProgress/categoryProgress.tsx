708f0558c57cab0cc4309916ce42948f0440af30
Stian have <stianhave@hotmail.com>
2022-07-27 20:20:44 +0200

    refactored code

--- sbanken/src/routes/categories/categoryProgress/categoryProgress.tsx
+++ sbanken/src/routes/categories/categoryProgress/categoryProgress.tsx
@@ -1,26 +1,15 @@
 import { useEffect, useState } from 'react';
-import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
-import { transactionCategory } from '../../../enums/transactionCategory';
-import { TransactionItem, TransactionWithCategoryItem } from '../../../interfaces/transactionItem';
+import { categoryItem } from '../../../interfaces/categoryItem';
+import { TransactionItem } from '../../../interfaces/transactionItem';
 import { useApiStore } from '../../../stores/useApiStore';
-import { TransactionBlackList } from '../../../utils/blackList';
-import { TransactionCategoryMapper } from '../../../utils/transactionCategoryMapper';
 import './categoryProgress.scss';
-
-type categoryItems = {
-    category: transactionCategory;
-    color: string;
-    count: number;
-    amountIn: number;
-    amountOut: number;
-    amount: number;
-    transactions: TransactionItem[];
-};
+import { ProgressCircle } from './progressCircle/progressCircle';
+import { getTransactionsWithCategories, getTransactionsGroupedInCategories } from './utils/categoryProgressUtils';
 
 export const CategoryProgress = () => {
     const { transactions_store } = useApiStore();
     const [transactions, setTransactions] = useState<TransactionItem[]>();
-    const [transactionCategories, setTransactionCategories] = useState<categoryItems[]>();
+    const [transactionCategories, setTransactionCategories] = useState<categoryItem[]>();
     const [totalAmount, setTotalAmount] = useState<number>(0);
     const [transactionGridOpen, setTransactionGridOpen] = useState<boolean>(false);
     useEffect(() => {
@@ -35,78 +24,20 @@ export const CategoryProgress = () => {
     }, [transactions]);
     return (
         <div className='category-process-component'>
-            {transactionCategories?.map((transaction) => {
-                //
-                const percentage = Math.round((transaction.amount / totalAmount) * 100);
-                return (
-                    <button
-                        className='progress-bar'
+            <div className='transactions'>
+                {transactionCategories?.map((transaction) => (
+                    <ProgressCircle
+                        transaction={transaction}
                         key={transaction.category}
-                        onClick={() => setTransactionGridOpen((prev) => !prev)}
-                    >
-                        <CircularProgressbarWithChildren
-                            value={percentage}
-                            styles={buildStyles({
-                                pathTransitionDuration: 0.5,
-                            })}
-                            strokeWidth={6}
-                        >
-                            <p>{`${Math.round(Math.abs(transaction.amount))}kr`}</p>
-                            <strong>{`${percentage}%`}</strong>
-                        </CircularProgressbarWithChildren>
-                        <h3>{`${transactionCategory[transaction.category]}`}</h3>
-                    </button>
-                );
-            })}
+                        setTransactionGridOpen={setTransactionGridOpen}
+                        totalAmount={totalAmount}
+                    />
+                ))}
+            </div>
             <div className={`transaction-grid ${transactionGridOpen ? 'open' : 'closed'}`}>
                 <button onClick={() => setTransactionGridOpen((prev) => !prev)}>CLOSE</button>
                 <h1>transaction view</h1>
             </div>
         </div>
     );
-};
-
-const getTransactionsGroupedInCategories = (transactions: TransactionWithCategoryItem[]) => {
-    //Return an array of categories calculated by transactions parameter
-    const result = transactions.reduce((acc: categoryItems[], curr) => {
-        const existingCategory = acc.filter((transaction) => transaction.category === curr.category)[0];
-        if (existingCategory) {
-            //Add curr to existing category
-            existingCategory.amount += curr.amount;
-            existingCategory.amountIn += curr.amount > 0 ? curr.amount : 0;
-            existingCategory.amountOut += curr.amount < 0 ? curr.amount : 0;
-            existingCategory.count++;
-            existingCategory.transactions.push(curr);
-        } else {
-            const categoryItem: categoryItems = {
-                category: curr.category,
-                color: 'temporary color',
-                count: 1,
-                amountIn: curr.amount > 0 ? curr.amount : 0,
-                amountOut: curr.amount < 0 ? curr.amount : 0,
-                amount: curr.amount,
-                transactions: [curr],
-            };
-            acc.push(categoryItem);
-        }
-        return acc;
-    }, []);
-    return result.sort((a, b) => a.amount - b.amount);
-};
-
-const getTransactionsWithCategories = (transactions: TransactionItem[]) => {
-    const result = transactions?.reduce((acc: TransactionWithCategoryItem[], curr) => {
-        const merchantName = curr.cardDetails?.merchantName;
-        const name = merchantName ? merchantName : curr.text;
-        if (TransactionBlackList.includes(name)) return acc;
-        const category = TransactionCategoryMapper(name);
-        const transactionItemWithCategory: TransactionWithCategoryItem = {
-            ...curr,
-            category: category,
-            name: name,
-        };
-        acc.push(transactionItemWithCategory);
-        return acc;
-    }, []);
-    return result;
 };
