6152444b94164cc4330fe4edbe96823aeef033c4
Stian have <stianhave@hotmail.com>
2022-07-28 22:05:15 +0200

    Refactored utils

--- sbanken/src/routes/home/cards/cards.tsx
+++ sbanken/src/routes/home/cards/cards.tsx
@@ -9,36 +9,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { useEffect, useState } from 'react';
 import { formatDateToIso } from '../../../services/formatDateToIso';
+import { getCardDates } from './localUtils/getCardDates';
+import { useApiStore } from '../../../stores/useApiStore';
 export const Cards = () => {
     const { endDate_store, startDate_store, setStartDate_store, setEndDate_store, setDate_store } = useDateStore();
-    const [monthsToMove, setMonthsToMove] = useState<number>(0);
+    const { transactions_store } = useApiStore();
+    const [monthsToMove, setMonthsToMove] = useState<number>(-1);
     const [nextMonthsName, setNextMonthsName] = useState<Array<string>>([]);
-    const daysInMonth = (month: number, year: number) => {
-        return new Date(year, month, 0).getDate();
-    };
+
     useEffect(() => {
-        const date = new Date();
-        const startDate = new Date(date.getFullYear(), date.getMonth() + monthsToMove, 1);
-        const endOfMonth = daysInMonth(new Date(startDate).getMonth(), new Date(startDate).getFullYear());
-        const endDate = new Date(date.getFullYear(), date.getMonth() + monthsToMove, endOfMonth - 1);
-        setStartDate_store(formatDateToIso(startDate));
-        setEndDate_store(formatDateToIso(endDate));
-        const selectionRange = {
-            startDate: startDate,
-            endDate: endDate,
-            key: 'selection',
-        };
-        setNextMonthsName([
-            new Date(startDate.setMonth(startDate.getMonth() - 1)).toLocaleString('default', { month: 'long' }),
-            new Date(startDate.setMonth(startDate.getMonth() + 2)).toLocaleString('default', { month: 'long' }),
-        ]);
-        setDate_store(selectionRange);
+        if (transactions_store) {
+            const { startDate, endDate, selectionRange, previousMonthName, nextMonthName } = getCardDates(monthsToMove);
+            setStartDate_store(formatDateToIso(startDate));
+            setEndDate_store(formatDateToIso(endDate));
+            setNextMonthsName([previousMonthName, nextMonthName]);
+            setDate_store(selectionRange);
+        }
     }, [monthsToMove]);
 
     const ChangeDate = (isLeft: boolean) => {
         if (isLeft) setMonthsToMove((prev) => (prev -= 1));
         if (!isLeft && monthsToMove < 0) setMonthsToMove((prev) => (prev += 1));
-        console.log('Changed date ', monthsToMove);
     };
 
     return (
@@ -78,7 +69,7 @@ export const Cards = () => {
                           })} - siste 30 dager`}
                 </p>
                 <button
-                    className={`arrow left ${monthsToMove === 0 ? 'disabled' : ''}`}
+                    className={`arrow left ${monthsToMove >= -1 ? 'disabled' : ''}`}
                     onClick={() => ChangeDate(false)}
                     title={nextMonthsName && nextMonthsName[1]}
                 >
