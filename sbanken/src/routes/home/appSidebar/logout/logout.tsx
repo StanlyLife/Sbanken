b487aa55592cd88999982edc22318e122e773308
Stian have <stianhave@hotmail.com>
2022-07-29 19:44:15 +0200

    added logout functionality

--- sbanken/src/routes/home/appSidebar/logout/logout.tsx
+++ sbanken/src/routes/home/appSidebar/logout/logout.tsx
@@ -1,10 +1,21 @@
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { useApiStore } from '../../../../stores/useApiStore';
+const loginObject = {
+    apiKey: '',
+    apiSecret: '',
+    isTestData: false,
+    isLoggedIn: false,
+};
 export const Logout = () => {
+    const { setLogin_store } = useApiStore();
+    const LogoutApp = () => {
+        console.log('logout');
+        setLogin_store(loginObject);
+    };
     return (
         <div className='log-out-component'>
-            <button className='logout'>
+            <button className='logout' onClick={LogoutApp}>
                 <FontAwesomeIcon icon={faArrowRightFromBracket} />
                 Logg ut
             </button>
