6534e9d4aa5d78ee5a66797dac190154a54d98c2
Stian have <stianhave@hotmail.com>
2022-07-29 19:35:30 +0200

    added login functionality

--- sbanken/src/App.tsx
+++ sbanken/src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { AccountResolver } from './initializers/accountResolver';
 import { authResolver } from './initializers/authResolver';
-import { TransactionResolver } from './initializers/transactionResolver';
-import { Home } from './routes/home/home';
 import './scss/main.scss';
 import './scss/fonts.scss';
 import 'react-circular-progressbar/dist/styles.css';
@@ -10,15 +7,27 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { Login } from './routes/login/login';
+import { useApiStore } from './stores/useApiStore';
+import { AccountResolver } from './initializers/accountResolver';
+import { TransactionResolver } from './initializers/transactionResolver';
+import { Home } from './routes/home/home';
 function App() {
     authResolver();
+    const { login_store } = useApiStore();
     return (
         <div className='App-Root'>
             <header className='App-header'></header>
             <main>
-                <Home />
-                <AccountResolver />
-                <TransactionResolver />
+                {login_store.isLoggedIn && login_store ? (
+                    <>
+                        <Home />
+                        <AccountResolver />
+                        <TransactionResolver />
+                    </>
+                ) : (
+                    <Login />
+                )}
             </main>
         </div>
     );
