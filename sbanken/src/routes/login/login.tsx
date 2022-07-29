bf727463ff923274fc75400bd5abde357d9e9a27
Stian have <stianhave@hotmail.com>
2022-07-29 22:55:16 +0200

    added automatic login, localstorage and error messages

--- sbanken/src/routes/login/login.tsx
+++ sbanken/src/routes/login/login.tsx
@@ -1,6 +1,6 @@
 import { faCircleQuestion } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useApiStore } from '../../stores/useApiStore';
 import './login.scss';
 
@@ -12,17 +12,42 @@ interface loginInformation {
 }
 
 export const Login = () => {
-    const { setLogin_store } = useApiStore();
+    const { setLogin_store, errorMessage_store } = useApiStore();
     const apiKeyInput = useRef<HTMLInputElement>(null);
     const apiSecretInput = useRef<HTMLInputElement>(null);
+
+    //initialize login if localstorage has been set
+    useEffect(() => {
+        const key = window.localStorage.getItem('apikey');
+        const secret = window.localStorage.getItem('apisecret');
+        if (!key || !secret) return;
+        //Set inputs to match local store
+        if (apiKeyInput.current) apiKeyInput.current.value = key;
+        if (apiSecretInput.current) apiSecretInput.current.value = secret;
+        const loginObject: loginInformation = {
+            apiKey: key,
+            apiSecret: secret,
+            isTestData: false,
+            isLoggedIn: false,
+        };
+        setLogin_store(loginObject);
+    }, []);
+
+    const setLocalStoreData = () => {
+        const key = apiKeyInput?.current?.value;
+        const secret = apiSecretInput?.current?.value;
+        if (!key || !secret) return;
+        window.localStorage.setItem('apikey', key);
+        window.localStorage.setItem('apisecret', secret);
+    };
     const Login = () => {
         const loginObject: loginInformation = {
             apiKey: apiKeyInput?.current?.value || '',
             apiSecret: apiSecretInput?.current?.value || '',
             isTestData: false,
-            isLoggedIn: true,
+            isLoggedIn: false,
         };
-        console.log(loginObject);
+        setLocalStoreData();
         setLogin_store(loginObject);
     };
     return (
@@ -45,6 +70,9 @@ export const Login = () => {
                         <div className='input'>
                             <input ref={apiSecretInput} /> <FontAwesomeIcon className='icon' icon={faCircleQuestion} />
                         </div>
+                    </div>
+                    <div className='errors'>
+                        {errorMessage_store.login && <p className='error'>{errorMessage_store.login}</p>}
                     </div>
                 </div>
                 <div className='buttons'>
