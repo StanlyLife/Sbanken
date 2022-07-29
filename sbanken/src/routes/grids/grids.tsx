a7e6cad277f908b39bd83b8cd381a7f67abbbc84
Stian have <stianhave@hotmail.com>
2022-07-29 23:12:14 +0200

    updated grid styling

--- sbanken/src/routes/grids/grids.tsx
+++ sbanken/src/routes/grids/grids.tsx
@@ -1,11 +1,19 @@
 import { TransactionGrid } from './transactionGrid/transactionGrid';
 import { TransactionSummaryGrid } from './transactionSummaryGrid/transactionSummaryGrid';
-
+import './grids.scss';
 export const Grids = () => {
     return (
-        <>
-            <TransactionSummaryGrid />
-            <TransactionGrid />
-        </>
+        <div className='grids-component'>
+            {/* Recent transactions */}
+            <div className='transaction-grid'>
+                <h2>Nylige transaksjoner</h2>
+                <TransactionGrid />
+            </div>
+            {/*Grouped transactions - Recurring transactions grouped into 1 column*/}
+            <div className='summary-grid'>
+                <h2>Grupperte transaksjoner</h2>
+                <TransactionSummaryGrid />
+            </div>
+        </div>
     );
 };
